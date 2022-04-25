dd7b65b90d52cf12dc52b1ba6f50d02f3a434c41
HegedusAlexandra <hexapastry@gmail.com>
2022-04-25 13:15:44 +0200

    1

---
+++ frontend/script.js
@@ -0,0 +1,28 @@
+const firstPerson = {
+    name:"peter",
+    sex:"male"
+}
+
+const secondPerson = {
+    name:"sarah",
+    sex:"female"
+}
+
+function detectSex(person) {
+    if(person.sex === "female"){
+        return "this person is a female"
+    } else if(person.sex === "male") {return "this person is a male"}
+    else { return "the person's sex not male nor female "}
+}
+
+
+console.log(detectSex(firstPerson));
+
+console.log(detectSex(secondPerson));
+
+console.log(detectSex( {
+    name:"penelope",
+    sex: "female2"
+}));
+
+// else után if(person.sex === "male") biztosít hogy csak akkor ezért undefined lesz female2 
